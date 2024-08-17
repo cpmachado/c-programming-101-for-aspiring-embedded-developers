38cd5222faeb2fa03eb17c441e02576b62b634ee
cpmachado <cpmachado@protonmail.com>
2024-08-17 21:55:09 +0100

    annotations

--- led-blink.c
+++ led-blink.c
@@ -2,14 +2,18 @@
  * Led blinker using ATMEGA 328p
  * Using pin B0
  **/
-#define F_CPU 8000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define F_CPU 8000000UL
+
 int main(void) {
+
+  /* Turn pin to ouput */
   DDRB |= _BV(DDB0);
 
+  /* On/Off every 0.5 s*/
   for (;;) {
     PORTB ^= _BV(PB0);
     _delay_ms(500);
